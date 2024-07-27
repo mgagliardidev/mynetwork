c7f249fe0df8ae80b37a12b4e0f461955412bc7a
mgagliardi.dev <mgagliardi.dev@gmail.com>
2024-07-28 01:45:13 +0200

    rm broken import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Header from "@/components/header";
-import GeneralLayout from "@/components/generalLayout";
 
 const inter = Inter({ subsets: ["latin"] });
 
