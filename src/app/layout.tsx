2d701285cb2e75e9695e61da7809cface90f86e3
mgagliardi.dev <mgagliardi.dev@gmail.com>
2024-07-29 22:38:36 +0200

    rm useless imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
 import Header from "@/components/header";
-import { BrowserRouter } from "react-router-dom";
 
 const inter = Inter({ subsets: ["latin"] });
 
