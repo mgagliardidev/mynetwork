71865eaa4c15402d8ef1f5837e305498920c5498
mgagliardi.dev <mgagliardi.dev@gmail.com>
2024-07-29 23:57:02 +0200

    fix naming

---
+++ src/types/careerCardProps.ts
@@ -0,0 +1,9 @@
+export type CareerCardProps = {
+    companyName: string;
+    roleName: string;
+    startDate: string;
+    endDate: string;
+    description: string;
+    skills: string[];
+  };
+  
